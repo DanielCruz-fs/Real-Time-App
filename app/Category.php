05c7dbdd74888d4568ff01a11150db4fe39277fa
daniel cruz <dannyfsociety@gmail.com>
2018-03-14 11:34:14 -0400

    CRUD API for Category

--- app/Category.php
+++ app/Category.php
@@ -9,6 +9,9 @@ class Category extends Model
      protected $table = 'categories';
      protected $fillable = ['name', 'slug'];
 
+     public function getRouteKeyName(){
+     	 return 'slug';
+     }
      public function questions(){
      	 return $this->hasMany('App\Question');
      }
