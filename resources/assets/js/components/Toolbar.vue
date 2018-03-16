897f238f349c818ab6be2bbe6ef91fb2ef264b26
daniel cruz <dannyfsociety@gmail.com>
2018-03-15 23:38:54 -0400

    Setting up Vuetify, VueRouter and Initial Components

---
+++ resources/assets/js/components/Toolbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-toolbar>
+    <!-- <v-toolbar-side-icon></v-toolbar-side-icon> -->
+    <v-toolbar-title>Nerdy</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>Blog</v-btn>
+      <v-btn flat>Preguntas</v-btn>
+      <v-btn flat>Categorias</v-btn>
+      <v-btn flat>Login</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+<script>
+ export default{
+
+ }  
+</script>
+<style>
+  
+</style>
